bf1f9d8bceb36059eb004b3cc6f41b12c9b0025a
eleftheros <eleftheros_all@protonmail.com>
2022-04-24 09:14:58 +0300

    Style Expense

--- src/lib/Expense.svelte
+++ src/lib/Expense.svelte
@@ -1,8 +1,90 @@
 <script>
   export let id = 0, text = '', amount = 0;
+
+  import { slide } from 'svelte/transition';
+
+  let amountShow = false;
+
+  const toggleShow = () => {
+    return amountShow = !amountShow;
+  };
 </script>
 
-<div>
-  <h2>{id + 1}. {text}</h2>
-  <p>Price: ${amount}</p>
-</div>
+<li class="item">
+  <div class="content">
+    <div class="box">
+      <h2 class="text">{text}</h2>
+      <button class="arrow" on:click={toggleShow}>
+        {#if amountShow}
+          ▲
+        {:else}
+          ▼
+        {/if}
+      </button>
+    </div>
+    <div class="btns">
+      <button class="edit btn">✎</button>
+      <button class="trash btn">🗑</button>
+    </div>
+  </div>
+  {#if amountShow}
+    <p transition:slide>Amount: ${amount}</p>
+  {/if}
+</li>
+
+<style>
+  .item {
+    width: 100%;
+    margin: 1rem 0;
+    background-color: rgba(0, 0, 0, .1);
+    padding: .5rem;
+    border-radius: .5rem;
+  }
+
+  .content {
+    display: flex;
+    align-items: center;
+  }
+
+  .box {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .text {
+    text-align: center;
+  }
+
+  .arrow {
+    margin-left: 1rem;
+    border: 0;
+    font-size: 1.2rem;
+    height: 2rem;
+    cursor: pointer;
+    background-color: transparent;
+    transform: translateY(-.3rem);
+    color: navy;
+  }
+
+  .btns {
+    flex-basis: 5rem;
+  }
+
+  .edit {
+    color: #066506;
+  }
+
+  .trash {
+    color: #ac2f48;
+  }
+
+  .btn {
+    border: 0;
+    font-size: 2.5rem;
+    height: 3rem;
+    cursor: pointer;
+    background-color: transparent;
+  }
+</style>
--- src/lib/Expenses.svelte
+++ src/lib/Expenses.svelte
@@ -4,14 +4,37 @@
   import Expense from './Expense.svelte';
 </script>
 
-<h2>Total Expenses {expenseLen}</h2>
-<p>Total Amount ${expenseTotal}</p>
-{#each expenses as expense (expense.id)}
-  <Expense {...expense} />
-{:else}
-  <p>No expenses</p>
-{/each}
+<ul class="expenses">
+  <h2 class="total">Total Expenses: {expenseLen}</h2>
+  <p class="amount">Total Amount: <span class="price">${expenseTotal}</span></p>
+  {#each expenses as expense (expense.id)}
+    <Expense {...expense} />
+  {:else}
+    <p>No expenses</p>
+  {/each}
+</ul>
 
 <style>
+  .expenses {
+    width: 80%;
+    max-width: 60rem;
+    margin: 1rem auto;
+    box-shadow: 1px 2px 3px #111;
+    padding: 1rem;
+  }
 
+  .total {
+    text-align: center;
+    font-size: 3rem;
+  }
+
+  .amount {
+    font-size: 2rem;
+    margin: 2rem auto;
+  }
+
+  .price {
+    font-weight: bolder;
+    font-size: 2.5rem;
+  }
 </style>
